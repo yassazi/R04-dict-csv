4c0b5a6c6abc7ee2c51cd577f0fc67e17e3d0e7a
Labidi Mohamed Yassine <6338384@cegepmontpetit.ca>
2026-02-12 14:53:09 -0500

    exo 0 fait

--- Ex0_dict.py
+++ Ex0_dict.py
@@ -12,7 +12,9 @@ print(f"Le dictionnaire étudiant est au départ: {etudiant}")
 
 print(f"Q1{80*'_'}")
 # Q1:  on veut savoir la valeur du nom de l'étudiant en utilisant les f string
-#      Dans le terminal, on aura: "Le nom de l'étudiant est : Vincent"  
+#      Dans le terminal, on aura: "Le nom de l'étudiant est : Vincent"
+print(f"Le nom de l'étudiant est : {etudiant['nom']}")
+
 
 
 
@@ -21,7 +23,8 @@ print(f"Q2{80*'_'}")
 # Q2: Ajoutez une nouvelle paire clé:valeur dans notre dictionnaire
 #  Ajoutez le courriel comme clé et '2112344@cegepmontpetit.ca'
 #  Dans le terminal on veut avoir: Voici le courriel de l'étudiant : 2112344@cegepmontpetit.ca
-
+etudiant["courriel"] = "2112344@cegepmontpetit.ca"
+print(f"Voici le courriel de l'étudiant : {etudiant['courriel']}")
 
 
 
@@ -32,6 +35,8 @@ print(f"Q3{80*'_'}")
 # Q3: Changez la valeur du courriel de l'étudiant
 # la nouvelle valeur '2000000@cegepmontpetit.ca'
 #  Dans le terminal on veut avoir: Voici le nouveau courriel de l'étudiant: 2000000@cegepmontpetit.ca
+etudiant["courriel"] = "2000000@cegepmontpetit.ca"
+print(f"Voici le courriel de l'étudiant : {etudiant['courriel']}")
 
 
 
@@ -51,7 +56,8 @@ print(f"Q5{80*'_'}")
 # Utilisez la méthode pop qui en plus d'enlever la clé, vous retourne la valeur qu'on enlève
 # Q5: Enlevez la clé 'age' de l'étudiant, mais imprimez la valeur qu'elle avait
 #      Dans le terminal on veut: On a enlevé l'âge de l'étudiant, sa valeur était: 17
-
+tempo = etudiant.pop("age")
+print(f"On a enlevé l'âge de l'étudiant, sa valeur était: {tempo}")
 
 
 
@@ -61,3 +67,5 @@ print(f"Q6{80*'_'}")
 # On peut obtenir le nombre de paires clés:valeurs dans notre dictionnaire avec la méthode len
 # Q6: Combient de paires clés:valeurs avons-nous maintenant dans notre dictionnaire?
 #      Dans le terminal: Nous avons maintenant X paires clés valeurs.
+nombre_tempo = len(etudiant)
+print(f" Nous avons maintenant {nombre_tempo} paires clés valeurs.")
